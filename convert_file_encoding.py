ccbca4c9dd8421885c5984b892dea55c2d39e978
Lykyk <787202867@qq.com>
2018-06-06 09:00:27 +0000

    添加编码转换脚本

---
+++ convert_file_encoding.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+#转换文件编码（linux版）
+import os
+
+# 转换文件编码
+def convert_file_encoding(source_file_path, source_encoding, target_file_path, target_encoding):
+    lines = open(source_file_path, encoding=source_encoding).readlines()  # 打开文件，读入每一行
+    print(source_file_path, "->" ,target_file_path)
+    fp = open(target_file_path, 'w', encoding=target_encoding)  # 打开要写的文件
+
+    for line in lines:  # 写入文件
+        fp.write(line)
+
+    fp.close()  # 关闭文件
+
+
+#　转换目录下文件的编码
+def convert_files_encoding(dir_path, new_dir_path, source_encoding, target_encoding):
+    filelist = os.listdir(dir_path)  # 该文件夹下所有的文件（包括文件夹）
+    for filename in filelist:  # 遍历所有文件
+        file_path = os.path.join(dir_path, filename)  # 原来的文件路径
+        if os.path.isdir(file_path):  # 如果是文件夹则跳过
+            continue
+        filename_extension = os.path.splitext(filename)[1]  # 获取文件扩展名
+        if filename_extension != ".java":    # 只修改扩展名为 .java 的文件，避免读取图片等二进制文件而导致的出错
+            continue
+        new_file_path = os.path.join(new_dir_path, filename)    # 新文件路径
+        convert_file_encoding(file_path, source_encoding, new_file_path, target_encoding)    # 转换文件编码
+
+
+
+if __name__ == '__main__':
+    source_encoding='gbk'
+    target_encoding='utf-8'
+    print(source_encoding, "转换为", target_encoding)
+    dir_path = input("输入源文件夹路径：")
+    new_dir_path = input("输入新文件夹路径：") 
+
+    convert_files_encoding(dir_path, new_dir_path, source_encoding, target_encoding)
